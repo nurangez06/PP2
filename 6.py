53765be2dd518bf35cbc06f89df312a95aea0bf0
nurangez06 <n_nurubloeva@kbtu.kz>
2025-02-01 11:06:28 +0500

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,15 @@
+#loop in list
+#prints every item from list separetly
+mylist = ["banana", "apples", "bread", "chocolate", "milk"]
+i = 0
+while i < len(mylist):
+  print(mylist[i])
+  i = i + 1
+
+myshopingbag = []
+#create new list and copy items from first list
+for i in mylist:
+  if "a" in i:
+    myshopingbag.append(i)
+
+print(myshopingbag) 
