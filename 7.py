c636eac54c3044aaae4891672d714947c2b81a93
Nurangez Nurubloeva <n_nurubloeva@kbtu.kz>
2025-02-07 18:29:03 +0500

    Delete 7.py

--- 7.py
+++
@@ -1,9 +0,0 @@
-#tupels
-
-mytuple = ("apple", "banana", "cherry", "apple", "cherry")
-#print(mytuple[:3]) #prints first four items
-
-x = list(mytuple)
-x.append("kiwi")
-mytuple = tuple(x)
-print(mytuple)
