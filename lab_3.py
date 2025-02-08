d8934feb94cb694a6382326955b7fadc94cfc6ce
Nurangez Nurubloeva <n_nurubloeva@kbtu.kz>
2025-02-08 10:41:26 +0500

    Update lab_3.py

--- lab_3.py
+++ lab_3.py
@@ -108,10 +108,8 @@ print(spy_game([1, 7, 2, 0, 4, 5, 0]))
 
 
 #9
-import math
-
 def sphere_volume(radius):
-    return (4/3) * math.pi * radius**3
+    return (4/3) * 3.14 * radius**3
 
 radius = float(input("Enter the radius of the sphere: "))
 volume = sphere_volume(radius)
